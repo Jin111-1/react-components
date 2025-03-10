580c43fb667487d8904700fe9be071bde77a1425
Jin react app <Passawit14@gmail.com>
2025-03-10 15:08:56 +0700

    Feat:finished

---
+++ src/Component/Button.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Section from "./Section-Component";
+function Button (){
+    return(
+        <button className="add-to-cart-button">Add to Cart</button>
+    )
+}
+export default Button;
---
+++ src/Component/Footer-Component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer(){
+    return(
+        <footer>
+        <p>&copy; 2023 Your Ecommerce Store. All rights reserved.</p>
+      </footer>
+    )
+}
+export default Footer;
